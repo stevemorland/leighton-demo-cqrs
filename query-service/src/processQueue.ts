559c17a1b05838aa37fd7fbd47128980910272b6
Steve Morland <smorland@gmail.com>
2022-07-12 12:32:59 +0100

    Queue amends

--- query-service/src/processQueue.ts
+++ query-service/src/processQueue.ts
@@ -1,11 +1,31 @@
 import { DynamoDB } from 'aws-sdk'
 
+const DocClient = new DynamoDB.DocumentClient()
+const TABLE_NAME = process.env.TABLE_NAME 
+
 export const handler = async (event) => {
 
-    for (let record of event.Records) {
+    if(TABLE_NAME) {
+        for (let record of event.Records) {
         
-        console.log("Record: ", record)
-
+            console.log("Record: ", record)
+    
+            const recordBody = JSON.parse(record.body)
+    
+            console.log(recordBody)
+    
+            const params = {
+                TableName: TABLE_NAME,
+                Item: {
+                    PK: `EMAIL#${recordBody.detail.email.toUpperCase()}`,
+                    SK: 'PROFILE',
+                    userID: recordBody.detail.userID,
+                    email: recordBody.detail.email
+                }
+            }
+    
+            await DocClient.put(params).promise()
+    
+        }
     }
-
 }
